581026e2c094da9682838566aad7a6308c7b4888
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2024-11-19 23:46:25 -0300

    sacadoerroreTS

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
-// Configuración de variables de entorno
-process.env.RESEND_API_KEY = "re_BuLeMtYd_6Tuyo4PwrxmMhXdcJK8Rumxy";
-process.env.RESEND_SENDER_EMAIL = 'tomy_ramos1991@yahoo.com.ar';
 
 export default defineConfig({
   integrations: [tailwind(), react()],
