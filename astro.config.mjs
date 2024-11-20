1add257627aa7d8a4858601f122953f79bc642a4
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2024-11-19 23:54:09 -0300

    vercellisto

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-
 
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
+  adapter: vercel(),
 });
